c0b157d636e04c38728371233e6160229b7cc813
Buyaki-Imint <masarabuyaki@gmail.com>
2022-03-16 13:39:46 +0300

    Let us try this

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *main -prints "_putchar" followed by new line
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	_putchar('_')
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C;
+	_putchar('p');
+	_putchar('u);
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
