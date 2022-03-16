c3abdceba948b919c6d08aa243017b9b59eebbb9
Buyaki-Imint <masarabuyaki@gmail.com>
2022-03-16 22:02:00 +0300

    Keep on trying

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,23 +2,23 @@
 #include "main.h"
 
 /**
- *main- prints _putchar
+ * main - prints Holberton
  *
- *Description: prints _putchar
+ * Description: prints Holberton
  *
- *Return 0
-*/
+ * Return: 0 (Success)
+ */
 
 int main(void)
 {
-        char text[9] = "_putchar";
-        int i = 0;
+	char text[10] = "Holberton";
+	int i = 0;
 
-        for (i = 0; i < 8; i++)
-        {
-                _putchar(text[i]);
-        }
-        _putchar('\n');
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
 
-        return (0);
+	return (0);
 }
