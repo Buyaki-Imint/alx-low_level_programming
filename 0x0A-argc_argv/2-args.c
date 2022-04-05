f830d82fbb736a7b37a3d1e1e96433194bcc8583
Buyaki-Imint <masarabuyaki@gmail.com>
2022-04-05 11:47:51 +0300

    Done

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -10,13 +10,13 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+        int i;
 
-	i = 0;
-	while (i < argc)
-	{
-		printf("%s\n", argv[i]);
-		i++;
-	}
-	return (0);
+        i = 0;
+        while (i < argc)
+        {
+                printf("%s\n", argv[i]);
+                i++;
+        }
+        return (0);
 }
